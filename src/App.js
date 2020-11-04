29dc93d87fe85a4b4c04bc4590807fcba6fde9a6
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-04 11:28:53 +0100

    Fix version-indicator

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
             <Home title={title} />
           </Route>
         </Switch>
-        <footer>Version Alpha 0.0.1</footer>
+        <p>Version Alpha 0.0.1</p>
       </Router>
     </>
   );
